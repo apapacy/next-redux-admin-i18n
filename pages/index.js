1028d7365db4f5afd308674d1d64dddd828cab2e
Andrey Ovcharenko <an6rey@gmail.com>
2017-10-28 03:31:08 +0300

    add redux

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,31 @@ import { I18Link } from '../components/i18link'
 import TranslatedPage  from '../components/translatedpage'
 import { translate } from 'react-i18next';
 import i18n from '../app/i18n';
+import {reduxPage} from '../app/redux';
+
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {namespaceConfig} from 'fast-redux'
+
+const DEFAULT_STATE = {build: 21}
+
+const {actionCreator, getState: getHomepageState} = namespaceConfig('homepage', DEFAULT_STATE)
+
+const bumpBuild = actionCreator(function bumpBuild (state, increment) {
+  return {...state, build: state.build + increment}
+})
+
+function mapStateToProps (state) {
+  return getHomepageState(state)
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ bumpBuild }, dispatch)
+}
 
 
+@reduxPage
+@connect(mapStateToProps, mapDispatchToProps)
 @translate(['page2', 'common'], { i18n, wait: process.browser })
 export default class Page extends TranslatedPage {
   render() {
@@ -17,6 +40,8 @@ export default class Page extends TranslatedPage {
       <a>here</a>
     </I18Link>{ props.userAgent }
     <p>scoped!</p>
+    <h3>Current build: {props.build}</h3>
+    <p><button onClick={(e) => props.bumpBuild(1)}>Bump build!</button></p>
   <style jsx>{`
     p {
       color: #fff;
